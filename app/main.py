78d6394d14d8c4ab12f325e4c5ddc9b332cb6d02
Josefina Santacruz <a01784675@tec.mx>
2026-06-09 22:18:35 +0200

    agregue middleware parapoder conectar el front con estaciones bien

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import init_db
-from app.routers import auth, predicciones, rutas, admin
+from app.routers import auth, predicciones, rutas, admin, estaciones
 
 app = FastAPI(title="Ecobici API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
     init_db()
 
-
 app.include_router(auth.router)
 app.include_router(predicciones.router)
 app.include_router(rutas.router)
 app.include_router(admin.router)
-
+app.include_router(estaciones.router)
 
 @app.get("/")
 def root():
